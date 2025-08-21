6978b3147aa52f44040cd2e35784864e3da7142a
lava76 <lava76+github@mail.cbw-clan.de>
2025-08-21 14:13:02 +0200

    Bumped version

--- check_expansion_market_variants.py
+++ check_expansion_market_variants.py
@@ -6,7 +6,7 @@ Checks and optionally fixes Expansion Market and Traders configuration JSON file
 """
 
 __author__ = "lava76"
-__version__ = "1.1.3"
+__version__ = "1.2.0"
 __license__ = "GPL-3.0-or-later"
 
 
